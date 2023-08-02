87939833443e2dfe4d6c41a074486421aae0ec66
Will Lauer <willlauer4@gmail.com>
2023-08-02 14:48:44 -0400

    Fixed Cursor From Showing

--- src/assets/index.js
+++ src/assets/index.js
@@ -74,9 +74,11 @@ function animateCircles() {
   let x = coords.x;
   let y = coords.y;
   
+  
   circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
+    let zindex = circles[index].zindex;
+    circle.style.left = x - 8 + "px";
+    circle.style.top = y - 8 + "px";
     
     circle.style.scale = (circles.length - index) / circles.length;
     
@@ -84,6 +86,7 @@ function animateCircles() {
     circle.y = y;
 
     const nextCircle = circles[index + 1] || circles[0];
+    nextCircle.zindex = zindex- 10;
     x += (nextCircle.x - x) * 0.3;
     y += (nextCircle.y - y) * 0.3;
   });
