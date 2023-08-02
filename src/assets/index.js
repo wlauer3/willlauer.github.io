013bb1074b05052f899e9bd2245c531da03469c8
Will Lauer <willlauer4@gmail.com>
2023-08-02 15:07:40 -0400

    Removed the gradient from cursor

--- src/assets/index.js
+++ src/assets/index.js
@@ -33,28 +33,28 @@ const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
 
 const colors = [
-  "#ffb56b",
-  "#fdaf69",
-  "#f89d63",
-  "#f59761",
-  "#ef865e",
-  "#ec805d",
   "#e36e5c",
-  "#df685c",
-  "#d5585c",
-  "#d1525c",
-  "#c5415d",
-  "#c03b5d",
-  "#b22c5e",
-  "#ac265e",
-  "#9c155f",
-  "#950f5f",
-  "#830060",
-  "#7c0060",
-  "#680060",
-  "#60005f",
-  "#48005f",
-  "#3d005e"
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c",
+  "#e36e5c"
 ];
 
 circles.forEach(function (circle, index) {
@@ -76,19 +76,18 @@ function animateCircles() {
   
   
   circles.forEach(function (circle, index) {
-    let zindex = circles[index].zindex;
+  
     circle.style.left = x - 8 + "px";
     circle.style.top = y - 8 + "px";
     
     circle.style.scale = (circles.length - index) / circles.length;
-    
     circle.x = x;
     circle.y = y;
 
     const nextCircle = circles[index + 1] || circles[0];
-    nextCircle.zindex = zindex- 10;
     x += (nextCircle.x - x) * 0.3;
     y += (nextCircle.y - y) * 0.3;
+    circle.zindex = nextCircle.zindex - 20;
   });
  
   requestAnimationFrame(animateCircles);
