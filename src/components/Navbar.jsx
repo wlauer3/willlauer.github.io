5ad4d56d3c0c16cb318eb73faf39effdc3dd2115
William Lauer <wlauer3@gatech.edu>
2024-05-24 10:28:49 -0400

    Small changes to nav link and titles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-12 h-12 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Will &nbsp;
+            William Lauer &nbsp;
             <span className='sm:block hidden'> | Electrical Engineer @ GeorgiaTech</span>
           </p>
         </Link>
